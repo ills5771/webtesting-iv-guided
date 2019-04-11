3adc5b89896db38fdb88d72880ee256d67141443
ills5771 <ilyayelizarov1@gmail.com>
2019-04-11 11:01:14 -0700

    latest changes

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,48 @@
 // Update with your config settings.
+const localPg = {
+  host: "localhost",
+  database: "hobbits",
+  user: "student",
+  password: "hired"
+};
+
+const productionDbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/hobbits.db3',
+      filename: "./data/hobbits.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
+  production: {
+    client: "pg",
+    connection: productionDbConnection, // could be an object or a string
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
